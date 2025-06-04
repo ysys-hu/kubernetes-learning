de95b726869b974e65f7f7e88ef68d67f85d3830
Kovács Dávid Artúr <artur@ysys.hu>
2025-06-04 18:46:57 +0200

    fix cluster installer

--- gke-cluster.sh
+++ gke-cluster.sh
@@ -2,9 +2,11 @@
 
 # This will dinamically set the project ID to the current project you are logged in
 PROJECT_ID=$(gcloud config get-value project)
-CLUSET_NAME="cluster-1"
-REGION="us-central1"
-KUBERNETES_VERSION="1.33.0-gke.2248000"
+CLUSET_NAME="intern-cluster"
+REGION="us-east1"
+KUBERNETES_VERSION="1.32.4-gke.1106006"
+NETWORK="default"
+SUBNEWORK="default"
 
 # Generate a basic cluster
 gcloud beta container \
@@ -13,19 +15,16 @@ gcloud beta container \
   --no-enable-basic-auth \
   --cluster-version $KUBERNETES_VERSION \
   --release-channel "regular" \
-  --machine-type "n2d-standard-2" \
-  --image-type "COS_CONTAINERD" \
-  --disk-type "pd-balanced" \
-  --disk-size "30" \
+  --machine-type "e2-medium" --spot --num-nodes "1" --image-type "COS_CONTAINERD" --node-locations "${REGION}-c" \
+  --disk-type "pd-standard" --disk-size "40" \
+  --enable-autoscaling --total-min-nodes "0" --total-max-nodes "2" --location-policy "BALANCED" \
   --metadata disable-legacy-endpoints=true \
   --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-  --spot \
-  --num-nodes "2" \
   --logging=SYSTEM,WORKLOAD \
   --monitoring=SYSTEM \
   --enable-ip-alias \
-  --network "projects/${PROJECT_ID}/global/networks/default" \
-  --subnetwork "projects/${PROJECT_ID}/regions/${REGION}/subnetworks/default" \
+  --network "projects/${PROJECT_ID}/global/networks/${NETWORK}" \
+  --subnetwork "projects/${PROJECT_ID}/regions/${REGION}/subnetworks/${SUBNEWORK}" \
   --no-enable-intra-node-visibility \
   --default-max-pods-per-node "110" \
   --enable-ip-access \
@@ -39,3 +38,6 @@ gcloud beta container \
   --max-surge-upgrade 1 \
   --max-unavailable-upgrade 0 \
   --enable-shielded-nodes \
+  --binauthz-evaluation-mode=DISABLED \
+  --shielded-integrity-monitoring \
+  --shielded-secure-boot
